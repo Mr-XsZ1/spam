5781c6c412285122554c5a9fb6ccbc49ee8a0056
Mr-XsZ1 <106481345+Mr-XsZ1@users.noreply.github.com>
2025-01-24 19:49:59 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import spamj, os
+import platform, os
 os.system("clear")
-spamj.ytno()
-spamj.main()
+
+if platform.architecture()[0] == '64bit':
+    import spama # file 64-bit
+    spama.ytno()
+    spama.main()
+else:
+    import spamj # file 32-bit
+    spamj.ytno()
+    spamj.main()
