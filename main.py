33db153c3e85dbfaea79930f9eb3f51eef71ee9e
Mr-XsZ1 <106481345+Mr-XsZ1@users.noreply.github.com>
2025-01-24 12:46:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import platform, os
-os.system("clear")
-
-if platform.architecture()[0] == '64bit':
-    import spama # file 64-bit
-    spama.ytno()
-    spama.main()
-else:
-    import spamj # file 32-bit
-    spamj.ytno()
-    spamj.main()
+import spamj
+spamj.ytno()
+spamj.main()
